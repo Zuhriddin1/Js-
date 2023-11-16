b04ca6677d1d0c5efe6639b4d0a1f08f25a79821
Zuhriddin1 <anarimon1985@gmail.com>
2023-11-16 19:44:25 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+//1-savol
+// let a = 1;
+// while (a < 11) {
+//   console.log(a);
+//   a++;
+// }
+// 2-savol
+// let a = 11;
+// while (101 > a) {
+//   console.log(a);
+//   a++;
+// }
+// 3-savol
+// let karra;
+// let yigindi = 0;
+// for (let i = 1; i < 11; i++) {
+//   yigindi = yigindi + i;
+// }
+// console.log("1 * 7 =7\n 2 * 7 = 14\n 3 * 7 = 27\n");
+// 4-savol
+// for (let i = 1; i < 11; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+// for (let i = 1; i < 11; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+// let yigindi = 0;
+// for (let i = 1; i < 11; i++) {
+//   yigindi = yigindi + i;
+// }
+// console.log(yigindi);
+// 5-savol
+// let karra = "";
+// for (let i = 1; i < 10; i++) {
+//   let ichkiKarra = "";
+//   for (let z = 2; z < 6; z++) {
+//     ichkiKarra = ichkiKarra + "\t" + z + "x" + i + " = " + z * i;
+//   }
+//   karra = karra + ichkiKarra + "\n";
+// }
+// karra = karra + "\n\n";
+// for (let i = 1; i < 10; i++) {
+//   let ichkiKarra = "";
+//   for (let z = 6; z < 10; z++) {
+//     ichkiKarra = ichkiKarra + "\t" + z + "x" + i + " = " + z * i;
+//   }
+//   karra = karra + ichkiKarra + "\n";
+// }
+// console.log(karra);
